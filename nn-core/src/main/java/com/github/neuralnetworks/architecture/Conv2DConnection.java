2484b444bae50aeb4e18f74b026cb6132a344ef9
ivanvasilev <ivanvasilev@gmail.com>
2013-12-27 22:56:05 +0200

    work-in-progress convolutional layers refactoring

--- nn-core/src/main/java/com/github/neuralnetworks/architecture/Conv2DConnection.java
+++ nn-core/src/main/java/com/github/neuralnetworks/architecture/Conv2DConnection.java
@@ -1,6 +1,5 @@
 package com.github.neuralnetworks.architecture;
 
-
 /**
  * Convolutional connection between layers (for 2d input data)
  */
@@ -10,13 +9,9 @@ public class Conv2DConnection extends ConnectionsImpl {
      * The list of filters to be used in the connection
      */
     protected float[] weights;
-    protected int kernelColumns;
-    protected int kernelRows;
     
     public Conv2DConnection(ConvGridLayer inputLayer, int kernelColumns, int kernelRows) {
 	super(inputLayer, new ConvGridLayer());
-	this.kernelColumns = kernelColumns;
-	this.kernelRows = kernelRows;
 	ConvGridLayer o = (ConvGridLayer) getOutputLayer();
 	o.updateDimensions();
     }
@@ -32,15 +27,7 @@ public class Conv2DConnection extends ConnectionsImpl {
     public void updateDimensions() {
 	ConvGridLayer i = (ConvGridLayer) getInputLayer();
 	ConvGridLayer o = (ConvGridLayer) getOutputLayer();
-	kernelColumns = i.getColumns() % o.getColumns() + 1;
-	kernelRows = i.getRows() % o.getRows() + 1;
-	updateWeights();
-    }
-
-    protected void updateWeights() {
-	ConvGridLayer i = (ConvGridLayer) getInputLayer();
-	ConvGridLayer o = (ConvGridLayer) getOutputLayer();
-	int totalWeights = kernelColumns * kernelRows * o.getFilters() * i.getFilters();
+	int totalWeights = getKernelColumns() * getKernelRows() * o.getFilters() * i.getFilters();
 	if (weights == null || weights.length != totalWeights) {
 	    weights = new float[totalWeights];
 	}
@@ -55,24 +42,26 @@ public class Conv2DConnection extends ConnectionsImpl {
     }
 
     public int getKernelColumns() {
-        return kernelColumns;
+        ConvGridLayer i = (ConvGridLayer) getInputLayer();
+	ConvGridLayer o = (ConvGridLayer) getOutputLayer();
+	return i.getColumns() % o.getColumns() + 1;
     }
 
     public void setKernelColumns(int kernelColumns) {
-        this.kernelColumns = kernelColumns;
-        updateWeights();
+        ConvGridLayer i = (ConvGridLayer) getInputLayer();
 	ConvGridLayer o = (ConvGridLayer) getOutputLayer();
-	o.updateDimensions();
+	o.setColumns(i.getColumns() - i.getColumns() % kernelColumns);
     }
 
     public int getKernelRows() {
-        return kernelRows;
+        ConvGridLayer i = (ConvGridLayer) getInputLayer();
+	ConvGridLayer o = (ConvGridLayer) getOutputLayer();
+	return i.getRows() % o.getRows() + 1;
     }
 
     public void setKernelRows(int kernelRows) {
-        this.kernelRows = kernelRows;
-        updateWeights();
+        ConvGridLayer i = (ConvGridLayer) getInputLayer();
 	ConvGridLayer o = (ConvGridLayer) getOutputLayer();
-	o.updateDimensions();
+	o.setRows(i.getRows() - i.getRows() % kernelRows);
     }
 }
